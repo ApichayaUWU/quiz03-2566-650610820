12e2c0a3fa031822ce48dd654d3e9bde9cac1c07
FAH <apichaya.inkhiewsai@gmail.com>
2023-09-14 15:07:11 +0700

    attempt1

--- src/app/api/message/route.js
+++ src/app/api/message/route.js
@@ -5,59 +5,104 @@ import { NextResponse } from "next/server";
 
 export const GET = async (request) => {
   readDB();
+  const roomId = request.nextUrl.searchParams.get("roomId");
+  //let message = null;
 
-  // return NextResponse.json(
-  //   {
-  //     ok: false,
-  //     message: `Room is not found`,
-  //   },
-  //   { status: 404 }
-  // );
+  let filtered = DB.messages;
+  const f = DB.messages.findIndex((std) => std.roomId === roomId);
+  if (roomId !== null) {
+    filtered = filtered.filter((std) => std.roomId === roomId);
+  }
+
+  if (f === -1) {
+    return NextResponse.json(
+      {
+        ok: false,
+        message: `Room is not found`,
+      },
+      { status: 404 }
+    );
+  }
+
+  return NextResponse.json({ ok: true, message: filtered });
 };
 
 export const POST = async (request) => {
+  const body = await request.json();
   readDB();
-
-  // return NextResponse.json(
-  //   {
-  //     ok: false,
-  //     message: `Room is not found`,
-  //   },
-  //   { status: 404 }
-  // );
+  const roomId = request.nextUrl.searchParams.get("roomId");
+  const f = DB.rooms.findIndex((std) => std.roomId === roomId);
+  if (f === -1) {
+    return NextResponse.json(
+      {
+        ok: false,
+        message: `Room is not found`,
+      },
+      { status: 404 }
+    );
+  }
 
   const messageId = nanoid();
+
+  DB.messages.push({
+    roomId,
+    messageId,
+    messageText,
+  });
 
   writeDB();
 
   return NextResponse.json({
     ok: true,
-    // messageId,
+    messageId: messageId,
     message: "Message has been sent",
   });
 };
 
 export const DELETE = async (request) => {
-  const payload = checkToken();
+  const body = await request.json();
 
-  // return NextResponse.json(
-  //   {
-  //     ok: false,
-  //     message: "Invalid token",
-  //   },
-  //   { status: 401 }
-  // );
+  let role = null;
+  let messageId = null;
+  //const payload = checkToken();
+  try {
+    const payload = checkToken();
+
+    messageId = payload.messageId;
+    role = payload.role;
+  } catch {
+    return NextResponse.json(
+      {
+        ok: false,
+        message: "Invalid token",
+      },
+      { status: 401 }
+    );
+  }
+
+  if (role !== "SUPER_ADMIN")
+    return NextResponse.json(
+      {
+        ok: false,
+        message: "Invalid token",
+      },
+      { status: 401 }
+    );
 
   readDB();
 
-  // return NextResponse.json(
-  //   {
-  //     ok: false,
-  //     message: "Message is not found",
-  //   },
-  //   { status: 404 }
-  // );
+  const f = DB.messages.findIndex((std) => std.messageId === body.messageId);
 
+  if (f === -1)
+    return NextResponse.json(
+      {
+        ok: false,
+        message: "Message is not found",
+      },
+      { status: 404 }
+    );
+
+  DB.messages.splice(f, 1);
   writeDB();
 
   return NextResponse.json({
--- src/app/api/room/route.js
+++ src/app/api/room/route.js
@@ -2,38 +2,67 @@ import { DB, readDB, writeDB } from "@/app/libs/DB";
 import { checkToken } from "@/app/libs/checkToken";
 import { nanoid } from "nanoid";
 import { NextResponse } from "next/server";
+import { z } from "zod";
 
-export const GET = async () => {
+export const GET = async (request) => {
   readDB();
+  const rooms = request.nextUrl.searchParams.get("rooms");
+
   return NextResponse.json({
     ok: true,
-    //rooms:
-    //totalRooms:
+    rooms: DB.rooms,
+    totalRooms: 2,
   });
 };
 
 export const POST = async (request) => {
-  const payload = checkToken();
+  const body = await request.json();
+  let username = null;
+  let role = null;
+  let roomName = null;
+  //const payload = checkToken();
+  try {
+    const payload = checkToken();
+    username = payload.username;
+    roomName = payload.roomName;
+    role = payload.role;
+  } catch {
+    return NextResponse.json(
+      {
+        ok: false,
+        message: "Invalid token",
+      },
+      { status: 401 }
+    );
+  }
 
-  // return NextResponse.json(
-  //   {
-  //     ok: false,
-  //     message: "Invalid token",
-  //   },
-  //   { status: 401 }
-  // );
+  if (role !== "ADMIN" && role !== "SUPER_ADMIN")
+    return NextResponse.json(
+      {
+        ok: false,
+        message: "Invalid token",
+      },
+      { status: 401 }
+    );
 
   readDB();
 
-  // return NextResponse.json(
-  //   {
-  //     ok: false,
-  //     message: `Room ${"replace this with room name"} already exists`,
-  //   },
-  //   { status: 400 }
-  // );
+  const froom = DB.rooms.find((r) => roomName === r.roomName);
+  if (froom)
+    return NextResponse.json(
+      {
+        ok: false,
+        message: `Room ${body.roomName} already exists`,
+      },
+      { status: 400 }
+    );
 
   const roomId = nanoid();
+
+  DB.rooms.push({
+    roomId,
+    roomName,
+  });
 
   //call writeDB after modifying Database
   writeDB();
@@ -41,6 +70,6 @@ export const POST = async (request) => {
   return NextResponse.json({
     ok: true,
     //roomId,
-    message: `Room ${"replace this with room name"} has been created`,
+    message: `Room ${body.roomName} has been created`,
   });
 };
